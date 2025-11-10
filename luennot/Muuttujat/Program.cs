98faf637ea5f4d9a8ca01bd5a0d032816f365e35
Juho S <juho.salli@gmail.com>
2025-11-10 11:35:39 +0200

    lisää muuttujat-luennon koodit

---
+++ luennot/Muuttujat/Program.cs
@@ -0,0 +1,25 @@
+﻿int age;
+
+age = 39;
+
+Console.WriteLine(age);
+
+int age2 = 20 + 2;
+Console.WriteLine(age2);
+age2 = age * 2;
+Console.WriteLine(age2);
+
+float pi = 3.14f;
+double pi2 = 3.14;
+decimal pi3 = 3.14M;
+Console.WriteLine(pi);
+
+bool isMotorRunning = true;
+isMotorRunning = false;
+Console.WriteLine(isMotorRunning);
+
+string firstname = "Juho";
+Console.WriteLine(firstname);
+
+string lause = "SAMK " + "on " + "koulu";
+Console.WriteLine(lause);
