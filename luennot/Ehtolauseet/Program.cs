a6889672d14b298e3ae2f9e44ca3ab3120c8fd19
Juho S <juho.salli@gmail.com>
2025-11-17 11:40:12 +0200

    lisää ehtolauseet-luennon koodit

---
+++ luennot/Ehtolauseet/Program.cs
@@ -0,0 +1,22 @@
+﻿bool a = true;
+bool b = false;
+if (a)
+{
+    Console.WriteLine("jepajee");
+}
+// int age = 19;
+
+// if (age < 18)
+// {
+//     Console.WriteLine("olet alle 18. Mene pois!");
+// }
+// else if (age == 18)
+// {
+//     Console.WriteLine("olet tasan 18 vuotta vanha!");
+// }
+// else
+// {
+//     Console.WriteLine("Olet yli 18");
+// }
+
+// Console.WriteLine(age);
