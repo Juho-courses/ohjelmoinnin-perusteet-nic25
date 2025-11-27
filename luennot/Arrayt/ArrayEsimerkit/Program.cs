7ae618040c61ccfdab3d59f32c61383ef04d81f8
Juho Salli <juho.salli@gmail.com>
2025-11-27 13:55:06 +0200

    lisää arrayluennon koodit

---
+++ luennot/Arrayt/ArrayEsimerkit/Program.cs
@@ -0,0 +1,22 @@
+﻿// 0 1 2 3 4
+int[] ints = new int[5];
+
+ints[0] = 123;
+ints[4] = 321;
+
+// for (int i = 0; i < ints.Length; i++)
+// {
+//     Console.WriteLine(ints[i]);
+// }
+
+string[] sanat = ["moi", "SAMK", "pori", "kolme"];
+// sanat[0] = "moi";
+// sanat[1] = "SAMK";
+// sanat[2] = "Pori";
+// sanat[3] = "kolme";
+foreach (string sana in sanat)
+{
+    Console.WriteLine(sana);
+}
+
+Console.WriteLine(sanat);
