a6bdaf9fab098e34c9a95387623cbbe0a08f6736
Juho Salli <juho.salli@gmail.com>
2025-11-27 09:07:38 +0200

    Lisää toistolauseet-luennon koodit sekä tyyppimuunnosesimerkin

---
+++ luennot/Toistolauseet/Program.cs
@@ -0,0 +1,29 @@
+﻿    // alustus; ehto; muutos
+// for (int i = 0; i < 10; i++)
+// {
+//    if (i == 3)
+//     {
+//         continue;
+//     }  
+//     Console.WriteLine(i);
+// }
+
+for (int asd = 10; asd > 0; asd -= 2)
+{
+    Console.WriteLine(asd);
+}
+
+// int counter = 0;
+
+// while (true)
+// {
+//     Console.WriteLine(counter);
+//     counter++;
+//     if (counter == 5)
+//     {
+//         break;
+//     }
+// }
+
+
+
---
+++ luennot/Tyyppimuunnos/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.Contracts;
+
+Console.Write("anna luku:");
+string syote = Console.ReadLine();
+
+int syote_int = int.Parse(syote);
+
+
+Console.WriteLine(syote_int * 2);
