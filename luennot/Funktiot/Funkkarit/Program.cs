652bbfbc804203d2973323c25bc0bec6010e040f
Juho S <juho.salli@gmail.com>
2025-12-04 13:49:23 +0200

    funktioluennon koodit

---
+++ luennot/Funktiot/Funkkarit/Program.cs
@@ -0,0 +1,18 @@
+﻿int TulostaNimiJaPalautaPituus(string sana)
+{
+    Console.WriteLine("Tulostetaan: " + sana);
+    int pituus = sana.Length;
+    return pituus;
+}
+
+int pituus = TulostaNimiJaPalautaPituus("moro");
+Console.WriteLine(pituus);
+
+// int TeePlusLasku(int a, int b)
+// {
+//     return a + b;
+//     Console.WriteLine("tätä ei nähdä ikinä");
+// }
+
+// int summa = TeePlusLasku(1, 2);
+// Console.WriteLine(summa);
---
+++ luennot/Funktiot/Satunnaisuus/Program.cs
@@ -0,0 +1,6 @@
+﻿Random rand = new Random();
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(rand.Next(0, 101));
+}
